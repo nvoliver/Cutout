ee5faf72bcc196f443556ae1030dc4da41cf81e4
oknieps <oknieps@nvidia.com>
2020-03-24 10:46:01 -0700

    Transform classification head into AdaptiveAvgPool2d and Conv1x1

--- model/resnet.py
+++ model/resnet.py
@@ -74,7 +74,11 @@ class ResNet(nn.Module):
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-        self.linear = nn.Linear(512*block.expansion, num_classes)
+        # Add global pooling:
+        self.global_pool = nn.AdaptiveAvgPool2d(1)
+        # Replace Linear layer with 1x1 Conv:
+        # self.linear = nn.Linear(512*block.expansion, num_classes)
+        self.linear = nn.Conv2d(512*block.expansion, num_classes, kernel_size=1, bias=True)
 
     def _make_layer(self, block, planes, num_blocks, stride):
         strides = [stride] + [1]*(num_blocks-1)
@@ -90,8 +94,11 @@ class ResNet(nn.Module):
         out = self.layer2(out)
         out = self.layer3(out)
         out = self.layer4(out)
-        out = F.avg_pool2d(out, 4)
-        out = out.view(out.size(0), -1)
+        # Replace fixed-window pooling with global pooling:
+        # out = F.avg_pool2d(out, 4)
+        out = self.global_pool(out)
+        # Replace Linear layer with 1x1 Conv:
+        # out = out.view(out.size(0), -1)
         out = self.linear(out)
         return out
 
